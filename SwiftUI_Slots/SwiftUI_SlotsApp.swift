4149acd922b49e063a83ad0664903849533cca98
Francesca Tyler <francesca.t.tyler@gmail.com>
2022-01-05 00:43:57 -0500

    Initial Commit

---
+++ SwiftUI_Slots/SwiftUI_SlotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_SlotsApp.swift
+//  SwiftUI_Slots
+//
+//  Created by Maxwell on 1/5/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_SlotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
